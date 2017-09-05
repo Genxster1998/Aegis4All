8821e806fc954da66c2963b6a2f7dbfe61d737bf
Karanvir Singh <karanvir.singh61@yahoo.in>
2017-09-05 10:46:34 +0530

    Fixing import errors

---
+++ app/src/main/java/com/mokke/aegis/activities/ManagePermissionsActivity.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2015-2016 The MoKee Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.mokke.aegis.activities;
+
+import android.app.AppOpsManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mokee.aegis.R;
+import com.mokee.aegis.fragments.HibernateAppsFragment;
+import com.mokee.aegis.fragments.PacifierAppsFragment;
+import com.mokee.aegis.fragments.PermissionAppsFragment;
+import com.mokee.aegis.fragments.WardenAppsFragment;
+
+public class ManagePermissionsActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.permissions_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(1);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            switch (position) {
+                case 0:
+                    return PermissionAppsFragment.newInstance(AppOpsManager.OP_WAKE_LOCK);
+                case 1:
+                    return WardenAppsFragment.newInstance();
+                case 2:
+                    return HibernateAppsFragment.newInstance();
+                case 3:
+                    return PacifierAppsFragment.newInstance();
+            }
+            return null;
+        }
+
+        public int getCount() {
+            return 4;
+        }
+
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.wakelock_title);
+                case 1:
+                    return getString(R.string.warden_title);
+                case 2:
+                    return getString(R.string.hibernate_title);
+                case 3:
+                    return getString(R.string.pacifier_title);
+            }
+            return null;
+        }
+    }
+}
